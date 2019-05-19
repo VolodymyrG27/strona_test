5370ae8381000c19c3f0e3e85373c59b6c1d0be2
Volodymyr Gotsal <vgotsak29@gmail.com>
2019-05-19 23:36:07 +0300

    galery vue

--- js/galery.js
+++ js/galery.js
@@ -1,12 +1,105 @@
-var locale = document.getElementsByClassName('locale_li'), i = 0;
-down.onclick = function() {
-    locale[i].style.display="none";
-    i = (i < (locale.length - 1))?i+1:0;
-    locale[i].style.display="block";
-}
+const groups = [
+    {
+        name: 'LODY',
+        items: [
+            {
+                src: 'img/fotkalodow.png',
+                alt: 'lody'
+            },
+            {
+                src: 'img/fotkalodow2.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow3.png',
+                alt: 'lody'
+            },
+            {
+                src: 'img/fotkalodow4.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow3.png',
+                alt: 'lody'
+            },
+            {
+                src: 'img/fotkalodow2.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow.png',
+                alt: 'lody'
+            }
+        ]
+    },
+    {
+        name: 'CIASTA',
+        items: [
+            {
+                src: 'img/fotkalodow4.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow3.png',
+                alt: 'lody'
+            },
+            {
+                src: 'img/fotkalodow2.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow.png',
+                alt: 'lody'
+            }
+        ]
+    },
+    {
+        name: 'DESERY',
+        items: [
+            {
+                src: 'img/fotkalodow2.png',
+                alt: 'lody2'
+            },
+            {
+                src: 'img/fotkalodow.png',
+                alt: 'lody'
+            }
+        ]
+    }
+]
 
-up.onclick = function() {
-    locale[i].style.display="none";
-    i = (i > 0)?i-1:locale.length - 1;
-    locale[i].style.display="block";
-}
+
+var galery = new Vue({
+    el: '#galeria',
+    data: {
+        pageNumber: 0,
+        groupImages: groups[0].items,
+        groups: groups
+    },
+    props: {
+        size: {
+            type: Number,
+            required: false,
+            default: 4
+        }
+    },
+    methods: {
+        selectPhoto(group) {
+            this.groupImages = group.items;
+            this.goToPage(0);
+        },
+        goToPage(page) {
+            this.pageNumber = page;
+        }
+    },
+    computed: {
+        pageCount() {
+            return Math.floor(this.groupImages.length / this.size);
+        },
+        paginatedData() {
+            const start = this.pageNumber * this.size;
+            const end = start + this.size;
+            return this.groupImages.slice(start, end);
+        }
+    }
+})
